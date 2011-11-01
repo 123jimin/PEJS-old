766929376fddfb0c26e3291928f20b4ae283fc10
JiminP <123jimin@naver.com>
2011-11-01 21:48:34 +0900

    Minor change.

--- FuncLib/General.js
+++ FuncLib/General.js
@@ -107,7 +107,7 @@ function strProd(a,b){
   a=''+a;b=''+b;
   if(a.length<b.length) return strProd(b,a);
   if(b.length==1) return prodDigit(a,~~b);
-  if(a.length<4) return ''+a*b;
+  if(a.length+b.length<10) return ''+a*b;
   else return strPlus(strProd(a,b.slice(0,b.length-1))+'0',prodDigit(a,~~b[b.length-1]));
 }
 function pow(a,b){
