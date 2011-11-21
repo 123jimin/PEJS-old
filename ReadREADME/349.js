b8d02e6763719e100d28ae9c6309f1e984836b36
JiminP <123jimin@naver.com>
2011-11-21 22:37:15 +0900

    JavaScript code for PE problem 349 added. Although it includes a little of pre-calculation (A cycle of 104, 10^18 mod 104, etc...), it actually calculates the answer anyway. (...) By 'solving' this problem using JavaScript, ALL problems I solved now can be solved by using only JS.

---
+++ ReadREADME/349.js
@@ -0,0 +1,27 @@
+function strPlus(a,b){
+  if(b=='0') return a; if(a=='0') return b;
+  if(b.length>a.length) return strPlus(b,a);
+  if(a.length<10) return ''+(~~a+~~b);
+  while(b.length<a.length) b='0'+b;
+  for(var s='',i=a.length-1,c=0,r;i>=0;i--){
+    if((r=~~a[i]+~~b[i]+c)>=10) c=1; else c=0;
+    s=(r%10)+s;
+  }
+  return c?'1'+s:s;
+}
+function prodDigit(a,d){
+  if(d==0) return '0'; if(d==1) return a;
+  for(var s='',i=a.length-1,c=0;i>=0;i--){
+    var r=a[i]*d+c; c=~~(r/10); r%=10;
+    s=r+s;
+  }
+  return c?c+s:s;
+}
+function strProd(a,b){
+  a=''+a;b=''+b;
+  if(a.length<b.length) return strProd(b,a);
+  if(b.length==1) return prodDigit(a,~~b);
+  if(a.length+b.length<10) return ''+a*b;
+  else return strPlus(strProd(a,b.slice(0,b.length-1))+'0',prodDigit(a,~~b[b.length-1]));
+}
+console.log(strPlus('772',strProd('12','9615384615384515'))); //9615384615384515 = (10^18 - 10440)/104
