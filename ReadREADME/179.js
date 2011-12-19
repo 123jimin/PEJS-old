4a646b6923c50a33df29985fb6e938ce4e647943
JiminP <123jimin@naver.com>
2011-12-20 00:38:08 +0900

    Changed algorithm used in 179.js

--- ReadREADME/179.js
+++ ReadREADME/179.js
@@ -1,5 +1,17 @@
-for(var a=new Uint16Array(1e7),i=2,j,k=0;i<1e7;i++){
-  for(j=i;j<1e7;j+=i) a[j]++;
-  if(a[i]==a[i-1]) k++;
+// Solution from Robert_Gerbicz
+var A=new Uint32Array(10000001),isPrime=new Uint8Array(3163),d=2,n,e,p,k,sum=0;
+for(n=2;n<3163;n++) isPrime[n]=1;
+for(;d<3163;d++) if(isPrime[d]){
+  for(n=d*d;n<3163;n+=d) isPrime[n]=0;
+  for(n=d*d;n<=10000000;n+=d) A[n]=d;
 }
-console.log(k);
+A[1]=1;
+for(n=2;n<=10000000;n++){
+  if(A[n]==0) A[n]=2; else{
+    p=A[n],k=n/p,e=2;
+    while(k%p==0){k/=p;e++;}
+    A[n]=A[k]*e;
+  }
+}
+for(n=2;n<10000000;n++) if(A[n]==A[n+1]) sum++;
+console.log(sum);
