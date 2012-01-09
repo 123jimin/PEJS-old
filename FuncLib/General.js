cb44dd0c41433f065511f32b4f4f2ca268d79ede
JiminP <123jimin@naver.com>
2012-01-09 19:38:54 +0900

    Modified isPrime to cover all integer range.

--- FuncLib/General.js
+++ FuncLib/General.js
@@ -5,12 +5,26 @@ function isPrime(n){
   var p=5; while(p*p<=n) if(n%p==0||n%(p+2)==0) return false; else p+=6;
   return true;
 }
-//Miller-Rabin (n<3e14)
+//Miller-Rabin (cover all integers)
+function modProd(a,b,n){
+  if(b==0) return 0;
+  if(b==1) return a%n;
+  return (modProd(a,(b-b%10)/10,n)*10+(b%10)*a)%n;
+}
+function modPow(a,b,n){
+  if(b==0) return 1;
+  if(b==1) return a%n;
+  if(b%2==0){
+    var c=modPow(a,b/2,n);
+    return modProd(c,c,n);
+  }
+  return modProd(a,modPow(a,b-1,n),n);
+}
 function isPrime(n){
   if(n==2||n==3||n==5) return true;
   if(n%2==0||n%3==0||n%5==0) return false;
   if(n<25) return true;
-  for(var a=[2,3,5,7,11,13,17],b=n-1,d,t,i,x;b%2==0;b/=2);
+  for(var a=[2,3,5,7,11,13,17,19],b=n-1,d,t,i,x;b%2==0;b/=2);
   for(i=0;i<a.length;i++){
     x=modPow(a[i],b,n);
     if(x==1||x==n-1) continue;
