320d9abe83cc967fcaeca277b272fbf9bea022f3
123jimin <123jimin@naver.com>
2012-03-31 18:00:36 +0900

    Even faster.

--- ReadREADME/045.js
+++ ReadREADME/045.js
@@ -1,10 +1,7 @@
-function isP(n){
-  n*=2; var i=~~((1+Math.sqrt(1+12*n))/6);
-  while(i*(3*i-1)<n) i++;
-  return i*(3*i-1)==n;
-}
-var n=143,n6;
-while(true){
-  n++; if(isP(n*(2*n-1))) break;
-}
-console.log(n*(2*n-1));
+for(var x,y,f=1,h,xy0=[[0,1],[0,-1],[1,-5],[1,5],[3,-19],[3,19]],i=0;f;)
+  for(i=0;f&&i<xy0.length;i++){
+    x=xy0[i][0],y=xy0[i][1];
+  if(x>0&&y>0&&y%6==5&&(h=x*(2*x-1))>40755) f=false;
+	xy0[i][0]=-7*x-y+2,xy0[i][1]=-48*x-7*y+12;
+  }
+console.log(h);
