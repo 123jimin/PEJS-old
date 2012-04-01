b4a671f7e58b9afc979eb1046ff7bc37fab2cd5e
123jimin <123jimin@naver.com>
2012-04-01 20:52:56 +0900

    I think I was crazy. A LOT..

--- ReadREADME/048.js
+++ ReadREADME/048.js
@@ -1,28 +1,14 @@
-function pdd(a,d){
-  if(d==0) return '0'; if(d==1) return a;
-  for(var s='',i=a.length-1,c=0;i>=0;i--){
-    var r=a[i]*d+c; c=~~(r/10); r%=10;
-    s=r+s;
+function modProd(a,b,n){
+  if(a*b<1e15) return a*b%n;
+  if(b%2==0) return 2*modProd(a,b/2,n)%n;
+  return (modProd(a,b-1,n)+a)%n;
+}
+function modPow(a,b,n){
+  if(b==0) return 1; if(b==1) return a;
+  if(b%2==0){
+    var c=modPow(a,b/2,n); return modProd(c,c,n);
   }
-  return c?c+s:s;
+  return modProd(modPow(a,b-1,n),a,n);
 }
-function pd(a,b){
-  return b.length==1?pdd(a,b):b.length==2?p(pdd(a,~~b[0])+'0',pdd(a,~~b[1])):
-  p(p(pdd(a,~~b[0])+'0',pdd(a,~~b[1]))+'0',pdd(a,~~b[2]));
-}
-function pow(a,b){
-  for(var s=''+a,i=0;i<b-1;i++) s=pd(s,''+a).slice(-10);
-  return s;
-}
-function p(a,b){
-  if(b==0) return a;
-  if(b.length>a.length) return p(b,a);
-  while(b.length<a.length) b='0'+b;
-  for(var s='',i=a.length-1,c=0,r;i>=0;i--){
-    if((r=~~a[i]+~~b[i]+c)>=10) c=1; else c=0;
-    s=(r%10)+s;
-  }
-  return c?'1'+s:s;
-}
-for(var s='1',i=2;i<1000;i++)s=p(s,pow(i,i));
-console.log(s.slice(-10));
+for(var i=1,s=0;i<=1000;i++) s=(s+modPow(i,i,1e10))%1e10;
+console.log(s);
