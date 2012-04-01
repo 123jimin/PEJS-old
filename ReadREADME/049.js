e742a204d64332ad975224b0bca2eba3edc4bff1
123jimin <123jimin@naver.com>
2012-04-01 20:44:42 +0900

    What the hell I was thinking?

--- ReadREADME/049.js
+++ ReadREADME/049.js
@@ -1,4 +1,4 @@
 function primeQ(n){if(n%2==0||n%3==0)return false;for(x=5;x<=Math.sqrt(n);x+=6)if(n%x==0||n%(x+2)==0) return false;return true;}
 function asdf(x){if(x<10)return (10+x);return asdf(x%10)*asdf(~~(x/10))}
-for(var i=1000,j;i<=9997;i++){if(i==1487||!primeQ(i))continue;for(j=i+2;2*j-i<9999;j++){
+for(var i=1001,j;i<=9997;i+=2){if(i==1487||!primeQ(i))continue;for(j=i+4;2*j-i<9999;j+=2){
 if(!primeQ(j)||!primeQ(2*j-i))continue;if(asdf(i)==asdf(j)&&asdf(j)==asdf(2*j-i))console.log(i+''+j+''+(2*j-i))}}
