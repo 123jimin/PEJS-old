51956d6cff696f96107a1632a9b2b969405aa879
libra guest <123jimin@naver.com>
2012-10-21 21:52:31 -0400

    JavaScript code for PE problem 175 added.

---
+++ ReadREADME/175.js
@@ -0,0 +1,18 @@
+function GCD(x,y){
+  if(x==0) return y;
+  if(y==0) return x;
+  if(x==1||y==1) return 1;
+  return GCD(y,x%y);
+}
+function _(x,y){
+  if(x==0||y==0) return [];
+  var i;
+  if(x>y) for(i=0;x>y;i++) x-=y;
+  else{i=Math.floor(y/x),y%=x}
+  return _(x,y).concat(i);
+}
+function $(x,y){
+  var d=GCD(x,y);
+  return _(x/d,y/d).join(',');
+}
+console.log($(123456789,987654321));
