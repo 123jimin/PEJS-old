a22dfd8b321b35e43b33e73acd9a8d66681a3e5a
Jimin Park <pjm950@gmail.com>
2013-09-16 21:31:49 +0900

    Changed to straightforward method.

--- ReadREADME/052.js
+++ ReadREADME/052.js
@@ -1,1 +1,4 @@
-console.log((1/7).toString().match(/\.(......)/)[1]);
+// console.log((1/7).toString().match(/\.(......)/)[1]);
+var x=10,s,h=function(x){return (x+'').split('').sort().join('');};
+for(;s=h(x);x++) if(s==h(2*x)&&s==h(3*x)&&s==h(4*x)&&s==h(5*x)&&s==h(6*x)) break;
+console.log(x);
